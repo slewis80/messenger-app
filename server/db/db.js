81d8e85ae9f0b582bac74a57933eef4e1f9f6a34
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2021-07-08 17:39:13 -0500

    initial setup and database connection completed

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "messenger", "postgres", "pw123!", {
+  dialect: 'postgres',
   logging: false
 });
+
 
 module.exports = db;
